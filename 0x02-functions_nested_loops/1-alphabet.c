5be669a34895a91c5b39dd579bd5797491db275d
vibez24 <jerrywordpressmail@gmail.com>
2022-09-13 23:55:02 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ *
+ * print allphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++ );
+	_putchar(alpha);
+	_putchar('\n');
+}
