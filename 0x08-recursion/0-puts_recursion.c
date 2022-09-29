856f7ba3833c9cb7249cc2f6a1d95c270bf91a72
vibez24 <jerrywordpressmail@gmail.com>
2022-09-28 18:10:48 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,16 +6,16 @@
  *@s : string parameter
  */
 
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 
 {
-	s = "rec\0";
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+        if (*s == '\0')
+        {
+                _putchar('\n');
+                return;
+        }
+        _putchar(*s);
+        s++;
+        _puts_recursion(s);
 }
+
