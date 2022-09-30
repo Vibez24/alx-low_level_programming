0c36c81b8fa3ecea2b4a4b5849101bcd77ec7fbb
vibez24 <jerrywordpressmail@gmail.com>
2022-09-30 12:33:02 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- *main - A program to print its name
- * @argc:argument counter
- * @argv:argument vector
+ * main - prints its name, followed by a new line
+ *
+ * @argc : counts arguments inputed in terminal
+ * @argv : argument vector
  * Return: 0 - success
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
