123a9a9f937e2173d4f722d39073ed4d1406d026
vibez24 <jerrywordpressmail@gmail.com>
2022-10-12 14:01:54 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *Struct_dog - a struct type for a dog poppy
+ * @ name first member
+ * @ age second member
+ * @ owner third member
+ *
+ */
+
+
+struct dog my_dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
