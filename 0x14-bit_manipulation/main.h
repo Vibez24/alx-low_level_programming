d4fb520aa66458678b32ba9b262df96eb4909a05
vibez24 <jerrywordpressmail@gmail.com>
2022-10-28 01:25:47 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
- * File: alx
- * AUth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
- */
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -15,6 +10,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
 
-#endif /* MAIN_H */
+#endif
